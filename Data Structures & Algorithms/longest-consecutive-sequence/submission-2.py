9d6b09249c08abb87b59b2c8da93ddcf97d772ba
Jeff Enriquez <30850773+Jeff-Enriquez@users.noreply.github.com>
2026-09-05 10:36:23 -0700

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.py
@@ -0,0 +1,38 @@
+"""Module to find the longest consecutive sequence in a list of integers.
+
+This module provides a class `Solution` with a method `longestConsecutive` that
+finds the length of the longest consecutive sequence in a list of integers.
+"""
+
+class Solution:
+    """Class to find the longest consecutive sequence in a list of integers."""
+
+    def longestConsecutive(self, nums: list[int]) -> int:
+        """Finds the length of the longest consecutive sequence in a list of integers.
+
+        Args:
+            nums: A list of integers.
+
+        Returns:
+            The length of the longest consecutive sequence.
+        """
+        num_map: dict[int, int] = {}
+        longest_streak = 0
+
+        for num in nums:
+            if num not in num_map:
+                # Length of previous number + length of current number + 1 (number we are adding to the length)
+                length = num_map.get(num - 1, 0) + num_map.get(num + 1, 0) + 1
+                num_map[num] = length
+
+                # Update left boundary
+                left_boundary = num - num_map.get(num - 1, 0)
+                num_map[left_boundary] = length
+
+                # Update right boundary
+                right_boundary = num + num_map.get(num + 1, 0)
+                num_map[right_boundary] = length
+
+                longest_streak = max(longest_streak, length)
+
+        return longest_streak
